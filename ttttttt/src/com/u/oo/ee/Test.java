afca135636df78302802e8bce30e99be55eafb0a
paula <paula@123.com>
2019-03-30 15:39:59 +0800

    dd

--- ttttttt/src/com/u/oo/ee/Test.java
+++
@@ -1,7 +0,0 @@
-package com.u.oo.ee;
-
-public class Test {
-	public static void main(String[] args) {
-		System.out.println("22");
-	}
-}
--- ttttttt/src/com/u/oo/hh/ttt.java
+++
@@ -1,7 +0,0 @@
-package com.u.oo.hh;
-
-public class ttt {
-	public static void main(String[] args) {
-		System.out.println("111");
-	}
-}
