9ed61df73738eed0cf6b7abc6d8b25448458e8c8
paula <paula@123.com>
2019-03-31 10:15:57 +0800

    Add description in OrderManageController.java

--- eshop/src/main/java/com/eshop/controller/backend/OrderManageController.java
+++ eshop/src/main/java/com/eshop/controller/backend/OrderManageController.java
@@ -15,9 +15,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * Created by paula
+ * Description: 
+ * This is the controller to handle the request on order management in background system:
+ * 		1.	List all the orders by page.
+ *		2.	Get detailed order by orderNo.
+ *		3.	Search order by orderNo .
+ *		4.	Set the order as shipped status.
+ * 
+ * @author Paula Lin
+ *
  */
-
 @Controller
 @RequestMapping("/manage/order")
 public class OrderManageController {
@@ -29,31 +36,61 @@ public class OrderManageController {
     @Autowired
     private IOrderService iOrderService;
 
-    @RequestMapping("list.do")
+    /**
+     * Description: 
+     * This method handler is to list all the orders by page.
+     * 
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+    @RequestMapping("list_all_orders.do")
     @ResponseBody
-    public ServerResponse<PageInfo> orderList(@RequestParam(value = "pageNum",defaultValue = "1") int pageNum,@RequestParam(value = "pageSize",defaultValue = "10")int pageSize){
+    public ServerResponse<PageInfo> listAllOrders(@RequestParam(value = "pageNum",defaultValue = "1") int pageNum,@RequestParam(value = "pageSize",defaultValue = "10")int pageSize){
     	return iOrderService.manageList(pageNum,pageSize);
     }
 
-    @RequestMapping("detail.do")
+    /**
+     * Description: 
+     * This method handler is to get detailed order by orderNo .
+     * 
+     * @param orderNo
+     * @return
+     */
+    @RequestMapping("get_order_detail.do")
     @ResponseBody
-  public ServerResponse<OrderVo> orderDetail(Long orderNo){
+  public ServerResponse<OrderVo> getOrderDetail(Long orderNo){
     	return iOrderService.manageDetail(orderNo);
     }
 
-
-    @RequestMapping("search.do")
+    /**
+     * Description: 
+     * This method handler is to search order by orderNo .
+     * 
+     * @param orderNo
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+    @RequestMapping("search_order.do")
     @ResponseBody
-    public ServerResponse<PageInfo> orderSearch(Long orderNo,@RequestParam(value = "pageNum",defaultValue = "1") int pageNum,
+    public ServerResponse<PageInfo> searchOrder(Long orderNo,@RequestParam(value = "pageNum",defaultValue = "1") int pageNum,
             @RequestParam(value = "pageSize",defaultValue = "10")int pageSize){
     	return iOrderService.manageSearch(orderNo,pageNum,pageSize);
     }
 
 
-
+    /**
+     * Description: 
+     * This method handler is to set the order as shipped status.
+     * 
+     * 发货
+     * @param orderNo
+     * @return
+     */
     @RequestMapping("send_goods.do")
     @ResponseBody
-    public ServerResponse<String> orderSendGoods(Long orderNo){
+    public ServerResponse<String> sendGoods(Long orderNo){
     	return iOrderService.manageSendGoods(orderNo);
     }
 }
